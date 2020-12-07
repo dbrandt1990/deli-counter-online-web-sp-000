d53518aef3ab39caf52d3242d4320321208ecfa7
Danny Brandt <dvbrandt90@gmail.com>
2020-12-07 15:50:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
 # Write your code here.
+
+def now_serving(line)
+  "There is nobody waiting to be served"
+end
+
+def take_a_number(line, name)
+  
+end
+
+def line(line)
+   if line.size == 0 
+     puts "The line is currently empty."
+   else 
+     results = "The line is currently:"
+     line.each_with_index do |person, i|
+       results << " #{i+1}. #{person} "
+     end
+     puts results.chomp
+   end
+end
